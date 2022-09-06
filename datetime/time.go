0c0af6d894be7ee2cda31c3a5a5a8d65bc682340
derin <ilter.derin@gmail.com>
2022-09-06 11:23:05 +0200

    add monthof

--- datetime/time.go
+++ datetime/time.go
@@ -2,11 +2,8 @@ package types
 
 import "time"
 
-const DateFormat = "02.01.2006"
-
 type Period struct {
-	Begin time.Time `json:"begin" yaml:"begin"`
-	End   time.Time `json:"end" yaml:"end"`
+	Begin, End time.Time
 }
 
 func StartOfDay(t time.Time) time.Time {
@@ -17,4 +14,14 @@ func StartOfDay(t time.Time) time.Time {
 func EndOfDay(t time.Time) time.Time {
 	nt := time.Date(t.Year(), t.Month(), t.Day(), 23, 59, 59, 0, time.UTC)
 	return nt
+}
+
+func MonthOf(t time.Time) Period {
+	var (
+		begin    = time.Date(t.Year(), t.Month(), 1, 0, 0, 0, 0, time.UTC)
+		finalDay = time.Date(t.Year(), t.Month()+1, 0, 0, 0, 0, 0, time.UTC).Day()
+		end      = time.Date(t.Year(), t.Month(), finalDay, 23, 59, 59, 0, time.UTC)
+	)
+
+	return Period{Begin: begin, End: end}
 }
